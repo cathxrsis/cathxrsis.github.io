0d7bf445d8bd7a4892d052fe6a408afd551cd2a1
Tom <tomwestbury@protonmail.com>
2024-08-18 21:47:41 +0100

    pre-index updater

--- site.hs
+++ site.hs
@@ -88,7 +88,7 @@ main = hakyllWith config $ do
     match "index.html" $ do
         route idRoute
         compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
+            posts <- fmap (take 1) . recentFirst =<< loadAllSnapshots "posts/*" "content"
             let indexCtx =
                     listField "posts" postCtx (return posts) `mappend`
                     constField "title" "Home"                `mappend`
